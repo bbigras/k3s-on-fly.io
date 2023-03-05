f351b8e698319cf24272dbb21407d3bc01270414
Bruno Bigras <bigras.bruno@gmail.com>
2023-03-05 16:48:58 -0500

    flake: set substituters

--- flake.nix
+++ flake.nix
@@ -1,5 +1,15 @@
 {
   description = "k3s-on-fly.io";
+  nixConfig.substituters = [
+    "https://cache.nixos.org"
+    "https://nix-community.cachix.org"
+    "https://pre-commit-hooks.cachix.org"
+  ];
+  nixConfig.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+  ];
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
