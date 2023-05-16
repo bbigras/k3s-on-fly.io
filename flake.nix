8f1fefd300e323fa224d2b9dc7f06c2de871e442
Bruno Bigras <bigras.bruno@gmail.com>
2023-05-15 21:26:40 -0400

    add cachix cache

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
     "https://cache.nixos.org"
     "https://nix-community.cachix.org"
     "https://pre-commit-hooks.cachix.org"
+    "https://k3s-on-fly.cachix.org"
   ];
   nixConfig.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+    "k3s-on-fly.cachix.org-1:mbTxv0x9GYNeLd7DL7R9Lmh+ruU1DOTHdRSz64HbXTg="
   ];
 
   inputs = {
